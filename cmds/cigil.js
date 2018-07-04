db599b62c29b32d14bd245d0e4cc340d21466e3e
AlexNSG <37541011+AlexNSG@users.noreply.github.com>
2018-07-04 11:31:40 +0600

    Update cigil.js

--- cmds/cigil.js
+++ cmds/cigil.js
@@ -1,11 +1,11 @@
 module.exports.run = async (bot, message, args) => {
     let role = message.guild.roles.find("name", "CIGIL");
-    if(message.member.roles.has(role.id)) return message.reply("у тебя уже есть роль CIGIL!");
+    if(message.member.roles.has(role.id)) return message.reply("Хорошо заберу обратно роль CIGIL, как хочешь!").then(message.member.removeRole('337228181662728194'));
     await message.member.addRole('337228181662728194')
 
     message.channel.send(`${message.author} получил роль CIGIL, введя команду !cigil`);
 }
-    
+
 module.exports.help = {
     name: "cigil"
 }
